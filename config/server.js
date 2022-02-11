03e64a1e718b507a7efde6657b69d410a135238a
nathzg <nathzg@gmail.com>
2022-02-11 13:56:29 +0000

    primer push de strapipg

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '767eb73a597eabb485251659a733bf72'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f902fbbf-c575-4b52-896e-c579460a5836'
+};
